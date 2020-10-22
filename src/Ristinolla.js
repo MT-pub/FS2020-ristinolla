50f676b49e2937152f1e41c79b5e2c65b3e0e989
Miika Tolonen <miika.s.tolonen@gmail.com>
2020-10-22 13:08:53 +0300

    Neliöt toimii värien kanssa

--- src/Ristinolla.js
+++ src/Ristinolla.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './Ristinolla.css';
-import { usetState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function Ristinolla() {
-  const [vuoro, setVuoro] = useState(x)
+  const [vuoro, setVuoro] = useState('x')
   useEffect(() => {
 
   },[vuoro])
 
-  const nelioNapautus = (iidee) {
+  const nelioNapautus = (nid) => {
+    if(document.getElementById(nid).innerHTML==='') {
+      document.getElementById(nid).innerHTML=vuoro
+      
+      if(vuoro === 'x') {
+        document.getElementById(nid).style="color:blue"
+        setVuoro('o')
+      } else {
+        document.getElementById(nid).style="color:red"
+        setVuoro('x')
+      }
+
+      tarkistaVoitto(nid)
+    }
+  }
+
+  const tarkistaVoitto = (nid) => {
     
   }
 
   return (
     <div className="Ristinolla">
       <div class="rivi">
-        <span class="nelio" id="n11" onClick={() => nelioNapautus(this.id)}></span>
-        <span class="nelio" id="n12" onClick={() => nelioNapautus(this.id)}></span>
-        <span class="nelio" id="n13" onClick={() => nelioNapautus(this.id)}></span>
+        <span class="Nelio" id="n11" onClick={() => nelioNapautus('n11')}></span>
+        <span class="Nelio" id="n12" onClick={() => nelioNapautus('n12')}></span>
+        <span class="Nelio" id="n13" onClick={() => nelioNapautus('n13')}></span>
       </div>
       <div class="rivi">
-        <span class="nelio" id="n21" onClick={() => nelioNapautus(this.id)}></span>
-        <span class="nelio" id="n22" onClick={() => nelioNapautus(this.id)}></span>
-        <span class="nelio" id="n23" onClick={() => nelioNapautus(this.id)}></span>
+        <span class="Nelio" id="n21" onClick={() => nelioNapautus('n21')}></span>
+        <span class="Nelio" id="n22" onClick={() => nelioNapautus('n22')}></span>
+        <span class="Nelio" id="n23" onClick={() => nelioNapautus('n23')}></span>
       </div>
       <div class="rivi">
-        <span class="nelio" id="n31" onClick={() => nelioNapautus(this.id)}></span>
-        <span class="nelio" id="n32" onClick={() => nelioNapautus(this.id)}></span>
-        <span class="nelio" id="n33" onClick={() => nelioNapautus(this.id)}></span>
+        <span class="Nelio" id="n31" onClick={() => nelioNapautus('n31')}></span>
+        <span class="Nelio" id="n32" onClick={() => nelioNapautus('n32')}></span>
+        <span class="Nelio" id="n33" onClick={() => nelioNapautus('n33')}></span>
       </div>
     </div>
   );
